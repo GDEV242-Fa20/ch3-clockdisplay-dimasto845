ad8f3d8db589949c7bfebd7ca6a6dfc61935639c
dimasto845 <dimkastomma@gmail.com>
2020-09-28 13:26:43 -0400

    lab clock display

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -17,11 +17,8 @@ public class ClockDisplay
     private NumberDisplay hours;
     private NumberDisplay minutes;
     private String displayString;    // simulates the actual display
-    
-    /**
-     * Constructor for ClockDisplay objects. This constructor 
-     * creates a new clock set at 00:00.
-     */
+
+    // Constructor for ClockDisplay objects.
     public ClockDisplay()
     {
         hours = new NumberDisplay(24);
@@ -29,35 +26,28 @@ public class ClockDisplay
         updateDisplay();
     }
 
-    /**
-     * Constructor for ClockDisplay objects. This constructor
-     * creates a new clock set at the time specified by the 
-     * parameters.
-     */
+    // Constructor for ClockDisplay objects.
     public ClockDisplay(int hour, int minute)
     {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
-
-    /**
-     * This method should get called once every minute - it makes
-     * the clock display go one minute forward.
-     */
+  
     public void timeTick()
     {
         minutes.increment();
-        if(minutes.getValue() == 0) {  // it just rolled over!
+        if(minutes.getValue() == 0) { // it just rolled over!
             hours.increment();
         }
         updateDisplay();
     }
 
-    /**
-     * Set the time of the display to the specified hour and
-     * minute.
-     */
+  
+    
+
+    //Set the time of the display to the specified hour and minute.
+   
     public void setTime(int hour, int minute)
     {
         hours.setValue(hour);
@@ -65,20 +55,35 @@ public class ClockDisplay
         updateDisplay();
     }
 
-    /**
-     * Return the current time of this display in the format HH:MM.
-     */
+    // Return the current time of this display in the format HH:MM.
+   
     public String getTime()
     {
         return displayString;
     }
-    
-    /**
-     * Update the internal string that represents the display.
-     */
+
+    // Update the internal string that represents the display.
+   
     private void updateDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+        int hour = hours.getValue();
+        String AmPm;
+
+        if( hour >= 12) {
+            AmPm= "PM";
+        }
+        else {
+            AmPm= "AM";
+        }
+
+        if (hour >= 12) {
+            hour -= 12;
+        }
+
+        if (hour == 00) {
+            hour = 12;
+        }
+
+        displayString = hour + ":" + minutes.getDisplayValue() + "."+AmPm;
     }
 }
